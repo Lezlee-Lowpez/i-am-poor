290a704bc4033fb9cfbe6e40e71e6a77ea4a8155
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-03-18 14:10:26 -0700

    Adding files

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I  Am Poor
+//
+//  Created by Lesley Lopez on 3/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
